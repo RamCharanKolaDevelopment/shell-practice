c20d49a53f34ead296951aee2c50848d8465b32a
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 09:53:06 +0530

    conversarion code

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hellow Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: Doing fine. What are you learning Ramesh?"
+echo "Ramesh:: I am learning shell. What about you?"
