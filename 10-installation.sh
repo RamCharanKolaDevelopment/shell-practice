6c9f82c04af6e15356369b4bfd15c328dff440d6
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 18:28:14 +0530

    without exit code

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ user=$(id -u)
 
 if [ $user -ne 0 ]; then
     echo "you should run this script with root user access to install nginx."
+    dnf install nginx -y
 elif [ $user -eq 0 ]; then
     dnf install nginx -y
 fi
