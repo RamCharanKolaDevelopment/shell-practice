dfa000ed6f5ac638a7c3c861eeb10ed67c1c47eb
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 13:39:46 +0530

    variable $ systax issue fix

--- 06-variables.sh
+++ 06-variables.sh
@@ -7,14 +7,14 @@
 # Shell executes: date +%s (which returns, for example, 1739778865).
 # Shell replaces: start_time=1739778865
 # Result: The variable start_time now holds the value 1739778865.
-start_time=(date +%s)
+start_time=$(date +%s)
 
 echo "Scripts executed at : $start_time"
 
 # used to keep linux system in sleep for 10 seconds.
 sleep 10
 
-end_time=(date +%s)
+end_time=$(date +%s)
 
 # (( )) double are used to tell linux this is Arithmetic Expansion.
 # $( ... ): Starts a Command Substitution (Run a command and put the result here). Note: In arithmetic expansion, it's combined as $((.
