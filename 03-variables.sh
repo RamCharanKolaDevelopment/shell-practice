8fe3c604cc2acfa1f6e58d26957043b997c3456d
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 10:15:54 +0530

    typo fixes

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@ person1="Chaitanya"  # In shell scripting for variable declaration there should 
 person2="Smantha"   # In shell scripting there should be no spaces around the = sign.
 
 # In shell scripting inside strings variables are accessed with "$<variable-name>"
-echo "$person1:: Hellow $person2, How are you?"
+echo "$person1:: Hello $person2, How are you?"
 echo "$person2:: Hi $person1, I am fine thanks for asking. How are you doing?"
 echo "$person1:: Doing fine. What are you learning $person2?" 
-echo "$person2:: I am learning shell. What about you?"
+echo "$person2:: I am learning shell scripting. What about you?"
