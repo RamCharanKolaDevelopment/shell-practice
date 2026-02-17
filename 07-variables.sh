b04f490fa41acafc3069382480629e415590b1dd
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 14:59:01 +0530

    special variables(special variables of shell script)

---
+++ 07-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# special variables
+echo "All arguments passed to the script $@"
+echo "Number of variables passed to script $#"
+echo "Script name $0"
+echo "Present working directory $PWD"
+echo "Who is running scripts $USER"
+echo "Home directory of current user $HOME"
+echo "PID of last run script $$"
+echo "PID of recently executed script ID $!"
+echo "All args passed to script $*"
+
+# $@ vs $*
+# ----------
+# $@ treats args separately
+# $* treats as single args
