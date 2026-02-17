9628d11c59231293fdb9ef92586130223df7fd9a
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 10:06:51 +0530

    variables declaration

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="Chaitanya"  # In shell scripting for variable declaration there should not be any white space between varaiable name & its value(around equal to symbol(=))
+person2="Smantha"   # In shell scripting there should be no spaces around the = sign.
+
+echo "$person1:: Hellow Ramesh, How are you?"
+echo "$person2:: Hi $person1, I am fine thanks for asking. How are you doing?"
+echo "$person1:: Doing fine. What are you learning $person2?"
+echo "$person2:: I am learning shell. What about you?"
