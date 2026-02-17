68273744d0a21d5be4a97384c02daa158d50c4f1
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 15:36:37 +0530

    Data types, number and string

---
+++ 08-data-types.sh
@@ -0,0 +1,11 @@
+#!.bin/bash
+
+# In shell scripting we have only 2 types of variable one is number, another one is string
+# Number data type variable declaration.
+num1=100
+num2=200
+# string data type variable declaration
+num3=ramcharan
+
+# performing Addition arithmetic operation
+echo "Sum of num1 & num2 is : $(($num1+$num2))"
