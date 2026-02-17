224bc76554ac3cf1e327898391463e73f4199387
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 15:07:39 +0530

    changes

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # special variables
-echo "All arguments passed to the script $@"
-echo "Number of variables passed to script $#"
-echo "Script name $0"
-echo "Present working directory $PWD"
-echo "Who is running scripts $USER"
-echo "Home directory of current user $HOME"
-echo "PID of last run script $$"
-echo "PID of recently executed script ID $!"
-echo "All args passed to script $*"
+echo "All arguments passed to the script : $@"
+echo "Number of variables passed to script : $#"
+echo "Script name : $0"
+echo "Present working directory : $PWD"
+echo "Who is running scripts : $USER"
+echo "Home directory of current user : $HOME"
+echo "PID of last run script : $$"
+
+sleep 100 &
+
+echo "PID of recently executed background process : $!"
+echo "All args passed to script : $*"
 
 # $@ vs $*
 # ----------
