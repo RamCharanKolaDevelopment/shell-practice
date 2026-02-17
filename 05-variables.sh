5eac9c0761231961a2dffc2551152820c7e387e8
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 13:05:35 +0530

    Reading user input & hiding user input

---
+++ 05-variables.sh
@@ -0,0 +1,17 @@
+#!ban/bash
+
+echo "Please Enter Your Name"
+
+# To read input from user.
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+read PASSWORD
+
+eccho "Password you entered is $PASSWORD"
+
+# To read user entered input without showing what he entered or To hide entered input
+read -s PASSWORD
+
+echo "Password you entered is $PASSWORD"
