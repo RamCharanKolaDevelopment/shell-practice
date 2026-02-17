1971e85821eb7f61c10a8029741f9c455d215bf6
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 09:27:17 +0530

    shell script "Hello world" 1st code.

---
+++ 01-hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# In shell scripting 1st line should be start with “Shebang” which is “#!/bin/bash” this text always should be in the 1st line in all your shell scripting files(.sh files). 
+# The “shebang” is a shell scripting interpreter of shell scripting code which checks and compile shell script code if any errors throughs error otherwise run your code and show output/execute script.
+# The # is used to write comment also. Except 1st line remaining all place hash(#) is used to write comments.
+
+echo "Hello world!"
+# “echo” is used as print, to print text like “Hello World” like print/println in python programming language.
