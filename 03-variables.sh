9f1fbf79519b16c8848e6b9f4224bab69be4e51a
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-17 10:13:49 +0530

    variables access inside string comment & person2 variable replace fix

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,8 @@
 person1="Chaitanya"  # In shell scripting for variable declaration there should not be any white space between varaiable name & its value(around equal to symbol(=))
 person2="Smantha"   # In shell scripting there should be no spaces around the = sign.
 
-echo "$person1:: Hellow Ramesh, How are you?"
+# In shell scripting inside strings variables are accessed with "$<variable-name>"
+echo "$person1:: Hellow $person2, How are you?"
 echo "$person2:: Hi $person1, I am fine thanks for asking. How are you doing?"
-echo "$person1:: Doing fine. What are you learning $person2?"
+echo "$person1:: Doing fine. What are you learning $person2?" 
 echo "$person2:: I am learning shell. What about you?"
