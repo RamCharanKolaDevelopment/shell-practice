c1388d5dcb8f6c4312958273be4395b3b2314d64
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-18 12:57:38 +0530

    creating logs in shell-practice folder with executed script file name

---
+++ 12-logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+user=$(id -u)
+log_folder="/var/log/shell-practice"
+log_file="/var/log/shell-practice/$0.log"
+
+if [ $user -ne 0 ]; then
+    echo "you should need root user access to run this script"
+    exit 1
+fi
+
+# To create shell-practice log folder.
+mkdir -p $log_folder
+
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+        echo "$1...FAILURE"
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+dnf install nginx -y &>> $log_file
+VALIDATE $? "installing nginx"
+
+dnf install nodejs -y &>> $log_file
+VALIDATE $? "installing nodejs"
+
+dnf install mysql -y &>> $log_file
+VALIDATE $? "installing mysql"
