64356341d8024bcef5047c6bf101ce87246fd5ce
RamCharanKolaDevelopment <ramcharankoladevelopment@gmail.com>
2026-02-18 13:23:18 +0530

    for loop - to print 10 numbers.

---
+++ 13-loops.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# for loop in shell-scripting
+for i in {1..10}
+do # starts code/statements execution
+    echo "$i"
+done # ends code/statements execution
+# the code b/w do & done got executed under for loop by shell
+
+
